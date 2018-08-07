9f7178a51aede5c4719d86595b0d885624e9218f
Stephanie Hu <stephaniehu@Stephanies-MacBook-Pro-2.local>
2018-08-06 21:03:19 -0700

    clean up

--- my_project/sketch.js
+++ my_project/sketch.js
@@ -36,7 +36,7 @@ window.onload=function(){
 		$("html, body").animate({ scrollTop: $(document).height() }, 4000);
 	}
 
-	function cdSelected(cdNum, selCdNum, cdDecorNum, bgnum, text, textNum){
+	function cdSelected(cdNum, selCdNum, cdDecorNum, bgnum, text, textNum,link){
 		document.getElementById("body").setAttribute("class", bgnum);
 		document.getElementById(cdNum).classList.add("selected");
 		list = document.getElementsByClassName("selectedCd");
@@ -49,12 +49,16 @@ window.onload=function(){
     		tlist[index].innerHTML = text;
 		}
 		$("#instructions").css("opacity",0);
-
-
+		
+ 	
 		slist = document.getElementsByClassName("spotify");
 		for (index = 0; index < slist.length; ++index) {
     		slist[index].id = "";
+    		slist[index].setAttribute('href',link);
+    		console.log("ok");
+    	
     	}
+
        	document.getElementById("decor").setAttribute("class",cdDecorNum);
        	$(window).on("scroll", function() {
   		$("." + cdDecorNum).css("opacity", (window.innerHeight + ($(window).scrollTop()* 100)) / ($(document).height()*40));
@@ -64,8 +68,10 @@ window.onload=function(){
 	$(window).scroll(function() {
    		if($(window).scrollTop() + $(window).height() > $(document).height() -120) {
 			$(".spotify").css("opacity", 1);
+			$(".cdText").css("opacity", 1);
 		}else{
 			$(".spotify").css("opacity", 0);
+			$(".cdText").css("opacity", 0);
 		}
 	});
 
@@ -73,47 +79,44 @@ window.onload=function(){
 	document.getElementById("cd1").addEventListener("click", cd1);
 	function cd1() {
 		clearSelection();
-		cdSelected("cd1","selectedCd1","decor1","bg1","i liked things better the way they were","cdText1");
+		cdSelected("cd1","selectedCd1","decor1","bg1","i liked things better the way they were","cdText1","https://open.spotify.com/user/123877546/playlist/0gLf0aS2jsms2QvBXoq3zU?si=OjIMk0YVSKyf214Y2bSJ-Q");
 		scrollBot()
 	}
 
 	document.getElementById("cd2").addEventListener("click", cd2);
 	function cd2() {
 		clearSelection();
-		cdSelected("cd2","selectedCd2","decor2","bg2","Humidity will make it feel like it's 100 degrees!","cdText2");
+		cdSelected("cd2","selectedCd2","decor2","bg2","Humidity will make it feel like it's 100 degrees!","cdText2","https://open.spotify.com/user/123877546/playlist/0tTiW4P4D19RDV3WhWSxrv?si=zb0GmsBXSXm7DVZ_iVaWCw");
 		scrollBot()
 	}
 
 	document.getElementById("cd3").addEventListener("click", cd3);
 	function cd3() {
 		clearSelection();
-		cdSelected("cd3","selectedCd3","decor3","bg3","one small step for mankind","cdText3");
+		cdSelected("cd3","selectedCd3","decor3","bg3","one small step for mankind","cdText3","https://open.spotify.com/user/123877546/playlist/71PhFS5UiafwQtpD0rhdHC?si=uHR2nnxzTYysQjzHWSyr1w");
 		scrollBot()
 	}
 
 	document.getElementById("cd4").addEventListener("click", cd4);
 	function cd4() {
 		clearSelection();
-		cdSelected("cd4","selectedCd4","decor4","bg4","i like the way it hurts", "cdText4");
+		cdSelected("cd4","selectedCd4","decor4","bg4","i like the way it hurts", "cdText4","https://open.spotify.com/user/123877546/playlist/0ujw5ajD3qoXjMnOkV8GRG?si=NGQRSV71R96_wFYOsqdauw");
 		scrollBot()	
 	}
 
 	document.getElementById("cd5").addEventListener("click", cd5);
 	function cd5() {
 		clearSelection();
-		cdSelected("cd5","selectedCd5","decor5","bg5", "youth knows no pain", "cdText5");
+		cdSelected("cd5","selectedCd5","decor5","bg5", "youth knows no pain", "cdText5","https://open.spotify.com/user/123877546/playlist/3Ht5ThZaYae8bCMnTDsIi6?si=OWzj7I2nTrqlW0qeWCLRzw");
 		scrollBot()
 	}
 
 	document.getElementById("cd6").addEventListener("click", cd6);
 	function cd6() {
 		clearSelection();
-		cdSelected("cd6","selectedCd6","decor6","bg6","why did you do it?", "cdText6");
+		cdSelected("cd6","selectedCd6","decor6","bg6","why did you do it?", "cdText6","https://open.spotify.com/user/123877546/playlist/60V8jut1wgBSzqWdUNu8Kf?si=ZrVqvOHAT_ae0WUzZzRlHA");
 		scrollBot()
 	}
-	$( ".spotify" ).click(function() {
-  	alert( "Handler for .click() called." );
-	});
 	// eject button
 
 	document.getElementById("ejectButton").addEventListener("click", eB);
